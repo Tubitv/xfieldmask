8d1a50c2ac318a37ac5687110ce38f189e938979
Balazs Szabo <1@dlux.cc>
2024-11-12 16:36:19 -0500

    🚧

--- src/main/scala/com/tubitv/xfieldmask/XFieldMaskCompanion.scala
+++ src/main/scala/com/tubitv/xfieldmask/XFieldMaskCompanion.scala
@@ -3,6 +3,6 @@ package com.tubitv.xfieldmask
 import com.tubitv.xfieldmask.XFieldMask
 
 trait XFieldMaskCompanion {
-  val Empty = XFieldMask()
-  val Full = XFieldMask(inverse = true)
+  val Empty = XFieldMask.defaultInstance
+  val Full = Empty.copy(inverse = true)
 }
